ed4d1653245d48fa982dfaeb687049816d9a4077
Xiaotian WU <XWu32@slb.com>
2022-10-25 10:39:30 +0800

    feat: add test

--- src/Foo.ts
+++ src/Foo.ts
@@ -1,5 +1,23 @@
 export class Foo {
   bar() {
-    return 'bar'
+    return "bar";
   }
+}
+
+export function isCuteNumberGreaterThan10(numbers: number[]) {
+  const eventNumber = findEvenNumbers(numbers);
+  const maxNumber = findMaxNumber(eventNumber);
+  return isNumberGreaterThan10(maxNumber);
+}
+
+export function findEvenNumbers(numbers: number[]) {
+  return numbers.filter((tmp) => tmp % 2 === 0);
+}
+
+export function findMaxNumber(number: number[]) {
+  return Math.max(...number);
+}
+
+function isNumberGreaterThan10(number: number) {
+  return number > 10;
 }
--- test/Foo.test.ts
+++ test/Foo.test.ts
@@ -1,7 +1,18 @@
-import {Foo} from '../src/Foo'
+import { Foo, isCuteNumberGreaterThan10 } from "../src/Foo";
 
-describe('Foo', () => {
-  it('bar', () => {
-    expect(new Foo().bar()).toEqual('bar')
-  })
-})
+describe("Foo", () => {
+  it("bar", () => {
+    expect(new Foo().bar()).toEqual("bar");
+  });
+
+  it("should return true given numbers: 1, 3, 5, 8, 12", () => {
+    // given
+    const numbers = [1, 3, 5, 8, 12];
+
+    // when
+    const res = isCuteNumberGreaterThan10(numbers);
+
+    // then
+    expect(res).toBeTruthy();
+  });
+});
